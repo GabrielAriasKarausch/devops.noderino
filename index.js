5f1b99fc948d33acedb945a1c6a3a185ee1e746c
GabrielAriasKarausch <gaboarias3108@gmail.com>
2025-06-11 09:04:48 +0000

    fix: export app for CI healthcheck

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const app = require("express")()
-const desiredPath = process.env.DESIRED_PATH || "/"
-const port = process.env.PORT || 80
-const number = process.env.NUMBER || 0
+const app = require("express")();
+const desiredPath = process.env.DESIRED_PATH || "/";
+const port = process.env.PORT || 80;
+const number = process.env.NUMBER || 0;
 
 app.get(desiredPath, (req, res) => {
-    res.send(`<h1>Hello from ${desiredPath} number ${number}!</h1>`)
-})
+  res.send(`<h1>Hello from ${desiredPath} number ${number}!</h1>`);
+});
 
 app.get("/healthcheck", (req, res) => {
-    res.status(200).send("It works!")
-})
+  res.status(200).send("It works!");
+});
 
-app.listen(port, () => {
-    console.log(`🚀 Server ${number} listening on ${port} at path ${desiredPath}`)
-})
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`🚀 Server ${number} listening on ${port} at path ${desiredPath}`);
+  });
+}
+
+module.exports = { app };
